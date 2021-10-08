f59d8f10f896dfa6a199827fc3e37f234651184a
Selma456 <selma.mikic@gmail.com>
2021-10-08 11:26:57 +0200

    add hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 const RepositoriesList: React.FC = () => {
+    const [term, setTerm] = useState('');
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setTerm(term);
+    };
+
     return (
         <div>
-            <form>
-                <input />
+            <form onSubmit={onSubmit}>
+                <input value={term} onChange={e => setTerm(e.target.value)} />
                 <button>Search</button>
             </form>
         </div>
