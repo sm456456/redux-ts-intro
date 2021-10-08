46c8158872709a25dcc3f1d5a9549f6e8055e24c
Selma456 <selma.mikic@gmail.com>
2021-10-08 10:35:52 +0200

    add reducer

--- src/state/action-creators/actionCreators.ts
+++ src/state/action-creators/actionCreators.ts
@@ -1,5 +1,37 @@
 import axios from 'axios';
+import { Dispatch } from 'react';
+import { ActionType } from '../action-types/actionTypes';
+import { Action } from '../actions';
 
-const searchRepositories = (term: string) => {
-    return async (dispach: any) => {};
+export const searchRepositories = (term: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES,
+        });
+
+        try {
+            const { data } = await axios.get(
+                'https://api.npms.io/v2/search?q=react',
+                {
+                    params: {
+                        text: term,
+                    },
+                }
+            );
+
+            const names = data.objects.map((result: any) => {
+                return result.package.name;
+            });
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names,
+            });
+        } catch (err: any) {
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                payload: err.message,
+            });
+        }
+    };
 };
